36d09bde4472cdd0dd58c3b1eb9cfa6b0e4aacd6
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-04-04 00:40:39 -0300

    feat(todo-list): defined the app structure

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,10 +1,18 @@
 import { Header } from './components/Header';
+
 import './global.css';
+import styles from './App.module.css';
 
 export function App() {
   return (
     <div>
       <Header />
+      <main className={styles.main}>
+        <input type="text" placeholder="Adicione uma nova tarefa" />
+        <div>
+          Tasks area
+        </div>
+      </main>    
     </div> 
   )
 }
