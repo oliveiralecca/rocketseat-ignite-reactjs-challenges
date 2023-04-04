e68ebe02c1663938cf25bae5407a03b9ff869f6e
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-04-04 00:26:02 -0300

    feat(todo-list): created the Header component

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,7 +1,10 @@
+import { Header } from './components/Header';
 import './global.css';
 
 export function App() {
   return (
-    <h1>ToDo List</h1>
+    <div>
+      <Header />
+    </div> 
   )
 }
