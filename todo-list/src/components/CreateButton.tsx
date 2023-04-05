a856f3597884e8e36c8436ec5358c7ebfe16e679
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-04-04 22:23:38 -0300

    feat(todo-list): created the CreateButton component and added the icons lib

---
+++ todo-list/src/components/CreateButton.tsx
@@ -0,0 +1,17 @@
+import { ReactElement, ReactNode } from "react";
+import { PlusCircle } from "phosphor-react";
+
+import styles from "./CreateButton.module.css";
+
+type CreateButtonProps = {
+  disabled: boolean;
+};
+
+export function CreateButton({ disabled }: CreateButtonProps) {
+  return (
+    <button className={styles.createButton} disabled={disabled}>
+      Criar
+      <PlusCircle size={20} />
+    </button>
+  );
+}
